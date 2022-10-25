78d34c0dae35ebc9d8c711154c6d9e58c50d5900
builder <builder@EECS-DIGITAL-22.MIT.EDU>
2022-10-25 12:07:48 -0400

    lets finish quick

---
+++ src/top_level.sv
@@ -0,0 +1,15 @@
+`default_nettype none
+`timescale 1ns / 1ps
+module top_level(
+	input wire clk,
+	input wire btnc,
+	input wire[1:0] eth_rxd,
+	input wire 
+
+	output logic[15:0] led,
+
+)
+
+
+`default_nettype wire
+
