cf000f53b033bd208d3786afbe1828ee76abf008
builder <builder@EECS-DIGITAL-22.MIT.EDU>
2022-10-27 12:16:32 -0400

    almost bitorder

--- sim/bitorder_tb.sv
+++ sim/bitorder_tb.sv
@@ -29,13 +29,14 @@ initial begin
 	$dumpfile("bitorder.vcd");
 	$dumpvars(0,bitorder_tb);
 	$display("Starting Sim");
-	clk = 0;
+	clk = 1;
 	#20;
 	rst = 1;
 	#20;
 	rst = 0;
-	axiiv = 1;
+	axiid <= 2'b11;
 	#10;
+	axiiv = 1;
 
 	for(int i = 0;i <4;i = i+1)begin
 		if(i < 3)begin
@@ -45,9 +46,36 @@ initial begin
 		end
 		#20;
 	end
+	#10;
+	axiiv = 0;
+	#100;
+	axiid = 2'b01;;
+	axiiv = 1;
+	#20;
+	axiid = 2'b11;
+	#20;
+	axiid = 2'b11;
+	#20;
+	axiid = 2'b11;
+	#20;
+	for(int i = 0;i < 4;i = i+1)begin
+		if(i < 3)begin
+                        axiid = 2'b01;
+                end else begin
+                        axiid = 2'b11;
+                end
+                #20;
+	end
 	axiiv = 0;
 	#100;
 	//test case 1, 1 byte
+	axiiv = 1;
+	for(int i = 0;i<2;i=i+1)begin
+		axiid = 2'b01;
+		#20;
+	end
+	axiiv = 0;
+	#100;
 	$display("Finishing Sim");
 	$finish;
 end
--- src/bitorder.sv
+++ src/bitorder.sv
@@ -14,9 +14,11 @@ module bitorder(
 logic [7:0] axiod_buffer_1;
 logic [7:0] axiod_buffer_2;
 logic buffer_switch; // if 0, buffer1 reads in. if 1 buffer2 reads in.
-logic[1:0] bit_count; //0th~3rd debit
-logic [10:0] byte_count; //how many bits 
+logic[2:0] bit_count; //0th~3rd debit
+logic [10:0] byte_count; //how ma:ny bits 
 logic old_axiiv;
+logic[1:0] rxd_out;
+logic[2:0] end_bit_count; //used for the last byte
 always_ff @(posedge clk)begin
 	if(rst)begin
 		axiov <= 0;
@@ -24,33 +26,40 @@ always_ff @(posedge clk)begin
 		buffer_switch <= 0;
 		bit_count <= 0;
 		byte_count <= 0;
+		old_axiiv <= 0;
+		axiod_buffer_1 <= 0;
+		axiod_buffer_2 <= 0;
+		end_bit_count <= 0;
 	end
 	else if(axiiv)begin
+		rxd_out <= axiid;
 		if(buffer_switch)
 			axiod_buffer_1 <= {axiid,axiod_buffer_1[7:2]};
 		else
 			axiod_buffer_2 <= {axiid,axiod_buffer_2[7:2]};
 
-		if(bit_count == 2'b11)begin //already read dibits three times
-			bit_count <= 2'b00;
-			byte_count = byte_count + 1;
+		if(bit_count == 3'd3)begin //already read dibits three times
+			bit_count <= 3'd4;
+			byte_count <= byte_count + 1;
 			buffer_switch <= ~buffer_switch;
+		end else if(bit_count == 3'd4)begin
+			bit_count <= 3'd1;
 		end else begin
 			bit_count <= bit_count + 1;
 		end
 		if(byte_count >= 1)begin
 			axiov <= 1;
 			case(bit_count)
-				2'b00:begin
+				3'd4:begin
 					axiod <= buffer_switch ? axiod_buffer_2[7:6] : axiod_buffer_1[7:6];
 				end
-				2'b01:begin
+				3'd1:begin
 					axiod <= buffer_switch ? axiod_buffer_2[5:4] : axiod_buffer_1[5:4];
 				end
-				2'b10:begin
+				3'd2:begin
                                         axiod <= buffer_switch ? axiod_buffer_2[3:2] : axiod_buffer_1[3:2];
                                 end
-				2'b11:begin
+				3'd3:begin
                                         axiod <= buffer_switch ? axiod_buffer_2[1:0] : axiod_buffer_1[1:0];
                                 end
 
@@ -60,31 +69,37 @@ always_ff @(posedge clk)begin
 		end
 		old_axiiv <= axiiv;
 	end else begin
-		if(old_axiiv)begin
-			case(bit_count)
-                                2'b00:begin
+		if(old_axiiv && bit_count == 3'd4)begin
+			case(end_bit_count)
+                                3'd0:begin
                                         axiod <= buffer_switch ? axiod_buffer_2[7:6] : axiod_buffer_1[7:6];
                                 end
-                                2'b01:begin
+                                3'd1:begin
                                         axiod <= buffer_switch ? axiod_buffer_2[5:4] : axiod_buffer_1[5:4];
                                 end
-                                2'b10:begin
+                                3'd2:begin
                                         axiod <= buffer_switch ? axiod_buffer_2[3:2] : axiod_buffer_1[3:2];
                                 end
-                                2'b11:begin
+                                3'd3:begin
                                         axiod <= buffer_switch ? axiod_buffer_2[1:0] : axiod_buffer_1[1:0];
                                 end
 
                         endcase
-			if(bit_count == 2'b11)begin
+			if(end_bit_count == 3'd4)begin
 				old_axiiv <= 0;
 				bit_count <= 0;
+				end_bit_count <= 0;
+				byte_count <= 0;
+				axiov <= 0;
 			end else begin
 				old_axiiv <= 1;
-				bit_count = bit_count + 1;
+				end_bit_count <= end_bit_count + 1;
+				axiov <= 1;
 			end
 		end
-		axiov <= 0;
+		else begin
+			axiov <= 0;
+		end
 	end
 end
 endmodule
